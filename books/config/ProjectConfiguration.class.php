7ba8b54a9fe60b0118f431297ab73848b53329b9
isao.ito <freesoftdev.isao@primo.repre.jp>
2012-11-29 22:33:02 +0900

    base

---
+++ books/config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/usr/local/hdp/php/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
